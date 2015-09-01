3e57d140494c8aa6b85a45e6222b6ed98f381a30
Masato Shimizu <ma6ato@gmail.com>
2015-09-01 16:21:12 +0900

    閾値とトリガー

--- bme280_publish.py
+++ bme280_publish.py
@@ -5,6 +5,7 @@ from time import sleep
 import json
 import sys
 import bme280
+import requests
 from config import conf
 
 def sensing():
@@ -17,15 +18,22 @@ def on_publish(client, userdata, mid):
     print("publish: {}".format(mid))
 
 def main():
+    # MQTT
     client = mqtt.Client(client_id='',
 	                 clean_session=True, protocol=mqtt.MQTTv311)
-
     client.username_pw_set(conf["TRIGGER_1_UUID"], conf["TRIGGER_1_TOKEN"])
-
     client.on_connect = on_connect
     client.on_publish = on_publish
-
     client.connect(conf["IDCF_CHANNEL_URL"], 1883, 60)
+
+    # HTTP
+    url = "http://{0}/data/{1}".format(conf["IDCF_CHANNEL_URL"],
+                                       conf["TRIGGER_1_UUID"]) 
+    headers = {
+        "meshblu_auth_uuid": conf["TRIGGER_1_UUID"],
+        "meshblu_auth_token": conf["TRIGGER_1_TOKEN"]
+    };
+    payload = {"trigger":"on"}
 
     while True:
         retval = sensing()
@@ -36,6 +44,9 @@ def main():
                               "payload": retval})
              print(message)
              client.publish("message",message)
+               
+             if retval["temperature"] > conf["THRESHOLD"]:
+                 r = requests.post(url, headers=headers, data=payload)
         sleep(5)
 
 if __name__ == '__main__':
