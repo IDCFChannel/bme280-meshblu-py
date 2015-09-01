730dd5e2b55c14f61b5239dfcfd5221a3569484b
Masato Shimizu <ma6ato@gmail.com>
2015-09-01 17:50:44 +0900

    閾値チェックトリガーのサンプル

---
+++ bme280_data.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from time import sleep
+import json
+import bme280
+import requests
+from config import conf
+
+def sensing():
+    return bme280.readData()
+
+def main():
+    # HTTP
+    url = "http://{0}/data/{1}".format(conf["IDCF_CHANNEL_URL"],
+                                       conf["TRIGGER_1_UUID"])
+    headers = {
+        "meshblu_auth_uuid": conf["TRIGGER_1_UUID"],
+        "meshblu_auth_token": conf["TRIGGER_1_TOKEN"]
+    };
+    payload = {"trigger":"on"}
+
+    while True:
+        retval = sensing()
+        if retval:
+             print(retval["temperature"])
+             if retval["temperature"] > conf["THRESHOLD"]:
+                 r = requests.post(url, headers=headers, data=payload)
+        sleep(5)
+
+if __name__ == '__main__':
+    main()
